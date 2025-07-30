a49352a91d12388f68454bcb24251688c7e93148
Palenzuelamora2 <daniel.palenzuela@juanxxiii.net>
2025-07-30 11:51:45 +0200

    Nuevo commit

--- src/ui/ErrorTooltip.jsx
+++ src/ui/ErrorTooltip.jsx
@@ -1,10 +1,8 @@
 
 import React, { useState } from 'react';
-import './ErrorTooltip.css'; 
+import './ErrorTooltip.css';
 
 const ErrorTooltip = ({ message, children }) => {
-  const [isVisible, setIsVisible] = useState(false);
-
   if (!message) {
     return children;
   }
@@ -12,16 +10,13 @@ const ErrorTooltip = ({ message, children }) => {
   return (
     <div
       className="error-tooltip-container"
-      onMouseEnter={() => setIsVisible(true)}
-      onMouseLeave={() => setIsVisible(false)}
     >
       {/* Usamos el children como referencia al input que necesitamos para utilizar el tooltip que es el que se renderiza */}
-      {children} 
-      {isVisible && (
-        <div className="error-tooltip-content">
-          {message}
-        </div>
-      )}
+      {children}
+      <div className="error-tooltip-content">
+        {message}
+      </div>
+
     </div>
   );
 };
